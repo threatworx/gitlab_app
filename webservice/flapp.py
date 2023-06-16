b498d10936caf04b8dd1de138a5625b79616d0a8
Ketan Nilangekar <ketonne@yahoo.com>
2023-06-16 20:13:57 +0000

    First commit

---
+++ webservice/flapp.py
@@ -0,0 +1,109 @@
+import os
+import sys
+import json
+import traceback
+
+from flask import Flask
+from flask import request, redirect
+
+from . import utils
+
+app = Flask(__name__)
+utils.set_requests_verify(os.path.dirname(os.path.realpath(__file__)) + os.sep + 'gd-ca-bundle.crt')
+
+@app.route('/')
+def index_page():
+    rurl = request.host_url+'configure'
+    return redirect(rurl, code=302)
+
+@app.route("/configure")
+def handle_configure_gitlab_app():
+    print("Configure app service")
+    config = utils.get_config()
+    if config['gitlab_app'].getboolean('setup_done'):
+        print("Warning app aervice is already setup")
+        file_path = os.path.dirname(os.path.realpath(__file__)) + "/../templates/setup_done.html"
+        with open(file_path, "r") as fd:
+            fc = fd.read()
+        return fc, 200, {'Content-Type': 'text/html'}
+
+    file_path = os.path.dirname(os.path.realpath(__file__)) + "/../templates/gitlab_app_config.html"
+    with open(file_path, "r") as fd:
+        fc = fd.read()
+    return fc, 200, {'Content-Type': 'text/html'}
+
+@app.route("/save_config", methods=['POST'])
+def handle_save_gitlab_app_config():
+    print("Save app service configuration")
+    config = utils.get_config()
+    if config['gitlab_app'].getboolean('setup_done'):
+        print("Warning app service is already setup")
+        file_path = os.path.dirname(os.path.realpath(__file__)) + "/../templates/setup_done.html"
+        with open(file_path, "r") as fd:
+            fc = fd.read()
+        return fc, 200, {'Content-Type': 'text/html'}
+
+    # update configuration
+    tw_handle = request.values.get('tw_handle')
+    tw_api_key = request.values.get('tw_api_key')
+    tw_instance = request.values.get('tw_instance')
+    sast_enabled = request.values.get('sast_enabled')
+    iac_enabled = request.values.get('iac_enabled')
+    secrets_enabled = request.values.get('secrets_enabled')
+    code_sharing_enabled = request.values.get('code_sharing_enabled')
+    tw_gl_host = request.values.get('tw_gl_host')
+    tw_gl_user = request.values.get('tw_gl_user')
+    tw_gl_access_token = request.values.get('tw_gl_access_token')
+    webhook_secret = request.values.get('tw_gl_webhook_secret')
+    tw_user_tags = request.values.get('tw_user_tags')
+    config['threatworx']['handle'] = tw_handle
+    config['threatworx']['token'] = tw_api_key
+    config['threatworx']['instance'] = tw_instance
+    config['gitlab_app']['gitlab_host'] = tw_gl_host
+    config['gitlab_app']['gitlab_user'] = tw_gl_user
+    config['gitlab_app']['gitlab_access_token'] = tw_gl_access_token
+    config['gitlab_app']['user_tags'] = tw_user_tags
+    config['gitlab_app']['sast_checks_enabled'] = 'true' if sast_enabled == 'yes' else 'false'
+    config['gitlab_app']['iac_checks_enabled'] = 'true' if iac_enabled == 'yes' else 'false'
+    config['gitlab_app']['secrets_checks_enabled'] = 'true' if secrets_enabled == 'yes' else 'false'
+    config['gitlab_app']['code_sharing'] = 'true' if code_sharing_enabled == 'yes' else 'false'
+    config['gitlab_app']['setup_done'] = 'true'
+    config['gitlab_app']['webhook_secret'] = webhook_secret
+    utils.write_config(config)
+    config = utils.get_config(True)
+
+    file_path = os.path.dirname(os.path.realpath(__file__)) + "/../templates/success.html"
+    with open(file_path, "r") as fd:
+        fc = fd.read()
+    return fc, 200, {'Content-Type': 'text/html'}
+
+@app.route("/webhook", methods=['POST'])
+def webhook():
+    try:
+        gitlab_token = None
+        secret = None
+        config = utils.get_config()
+        if 'webhook_secret' in config['gitlab_app']:
+            secret = config['gitlab_app']['webhook_secret']
+        if 'X-Gitlab-Token' in request.headers:
+            gitlab_token = request.headers['X-Gitlab-Token']
+
+        if gitlab_token and not secret:
+            print('Cannot verify GitLab webhook - missing secret')
+            return "Cannot verify GitLab webhook - missing secret", 400, {'Content-Type': 'text/plain'}
+        if gitlab_token and (gitlab_token != secret):
+            print('Webhook request failed verification')
+            return "Webhook request failed verification", 400, {'Content-Type': 'text/plain'}
+
+        #base_discovery_enabled = config['gitlab_app'].getboolean('base_discovery_enabled')
+        event = json.loads(request.data)
+        if event['event_name'] == 'push':
+            utils.process_push_request(event)
+
+        return "", 200, {'Content-Type': 'text/plain'}
+    except Exception as exc:
+        traceback.print_exc(file=sys.stderr)
+        return "Internal Server Error", 500, {'Content-Type': 'text/plain'}
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int("80"))
---
+++ webservice/utils.py
@@ -0,0 +1,159 @@
+import sys
+import os
+import subprocess
+import configparser
+import requests
+import time
+import uuid
+import tempfile
+import json
+
+config = None
+GoDaddyCABundle = True
+CONFIG_FILE = '/opt/tw_gitlab_app/config/config.ini'
+
+def get_config(force_read = False):
+    global config
+    if force_read == False and config is not None:
+        return config
+    global CONFIG_FILE
+    env_config_path = os.environ.get("TW_GITLAB_APP_CONFIG")
+    if env_config_path is None:
+        print("Warning environment variable [TW_GITLAB_APP_CONFIG] is not specified. Falling back to default path for config file [/opt/tw_gitlab_app/config/config.ini]")
+    elif os.path.isdir(env_config_path) == False:
+        print("Error specified path [%s] in environment varliable [TW_GITLAB_APP_CONFIG] not found" % env_config_path)
+        print("Error unable to start server...")
+        sys.exit(1)
+    else:
+        CONFIG_FILE = env_config_path + os.path.sep + "config.ini"
+
+    if os.path.isfile(CONFIG_FILE) == False:
+        print("Error configuration file [%s] not found" % CONFIG_FILE)
+        sys.exit(1)
+    config = configparser.ConfigParser()
+    config.read(CONFIG_FILE)
+    return config
+
+def write_config(config):
+    with open(CONFIG_FILE, 'w') as fd:
+        config.write(fd)
+
+def discover_repo(repo_url, asset_id, branch):
+    config = get_config()
+
+    gitlab_user = config['gitlab_app']['gitlab_user']
+    gitlab_access_token = config['gitlab_app']['gitlab_access_token']
+    updated_repo_url = "https://" + gitlab_user + ":" + gitlab_access_token + '@' + repo_url.split('//')[1]
+
+    handle = config['threatworx']['handle']
+    token = config['threatworx']['token']
+    instance = config['threatworx']['instance']
+
+    base_discovery = True
+    sast_checks_enabled = config['gitlab_app'].getboolean('sast_checks_enabled')
+    iac_checks_enabled = config['gitlab_app'].getboolean('iac_checks_enabled')
+    secrets_checks_enabled = config['gitlab_app'].getboolean('secrets_checks_enabled')
+    code_sharing = config['gitlab_app'].getboolean('code_sharing')
+    ssl_verification = config['threatworx'].getboolean('ssl_verification', fallback=True)
+    insecure = "" if ssl_verification else "--insecure"
+    dev_null_device = open(os.devnull, "w")
+    org = repo_url.split('//')[1].split('/')[1]
+    tags = config['gitlab_app'].get('user_tags')
+    tags = '' if tags is None else tags.strip()
+    tags = tags.split(',')
+    ptags = "--tag '%s'" % (org)
+    for tag in tags:
+        tag = tag.strip()
+        if tag == "":
+            continue # skip empty tags
+        ptags = "%s --tag '%s'" % (ptags, tag)
+
+    if base_discovery:
+        twigs_cmd = "twigs -v %s --handle '%s' --token '%s' --instance '%s' %s --create_empty_asset --apply_policy SYNC_SCAN --run_id gitlab_app repo --repo '%s' --assetid '%s' --assetname '%s'" % (insecure, handle, token, instance, ptags, updated_repo_url, asset_id, asset_id)
+        print("Starting asset discovery & scan for repo [%s] and branch [%s]" % (repo_url, branch))
+        if branch is not None:
+            twigs_cmd = twigs_cmd + " --branch '%s'" % branch
+
+        # Run base asset discovery
+        try:
+            out = subprocess.check_output([twigs_cmd], stderr=dev_null_device, shell=True)
+            print("Base asset discovery completed")
+        except subprocess.CalledProcessError as e:
+            print("Error running twigs discovery")
+            print(e)
+            return False
+
+    # Perform IaC checks if enabled and specified to be run
+    if iac_checks_enabled:
+        twigs_cmd = "twigs -v %s --handle '%s' --token '%s' --instance '%s' %s --create_empty_asset --no_scan --run_id gitlab_app repo --repo '%s' --assetid '%s' --assetname '%s' --iac_checks" % (insecure, handle, token, instance, ptags, updated_repo_url, asset_id, asset_id)
+        if branch is not None:
+            twigs_cmd = twigs_cmd + " --branch '%s'" % branch
+        if code_sharing == False:
+            twigs_cmd = twigs_cmd + " --no_code"
+        print("Running IaC checks for repo [%s] and branch [%s]" % (repo_url, branch))
+        try:
+            out = subprocess.check_output([twigs_cmd], stderr=dev_null_device, shell=True)
+            print("IaC checks completed")
+        except subprocess.CalledProcessError as e:
+            print("Error running twigs IaC checks")
+            print(e)
+            return False
+        
+    # Perform secrets checks if enabled
+    if secrets_checks_enabled:
+        twigs_cmd = "twigs -v %s --handle '%s' --token '%s' --instance '%s' %s --create_empty_asset --no_scan --run_id gitlab_app repo --repo '%s' --assetid '%s' --assetname '%s' --secrets_scan" % (insecure, handle, token, instance, ptags, updated_repo_url, asset_id, asset_id)
+        if branch is not None:
+            twigs_cmd = twigs_cmd + " --branch '%s'" % branch
+        if code_sharing == False:
+            twigs_cmd = twigs_cmd + " --no_code"
+        print("Running secrets checks for repo [%s] and branch [%s]" % (repo_url, branch))
+        try:
+            out = subprocess.check_output([twigs_cmd], stderr=dev_null_device, shell=True)
+            print("Secrets checks completed")
+        except subprocess.CalledProcessError as e:
+            print("Error running twigs secrets checks")
+            print(e)
+            return False
+
+    # Perform sast checks if enabled
+    if sast_checks_enabled:
+        twigs_cmd = "twigs -v %s --handle '%s' --token '%s' --instance '%s' %s --create_empty_asset --no_scan --run_id gitlab_app repo --repo '%s' --assetid '%s' --assetname '%s' --sast" % (insecure, handle, token, instance, ptags, updated_repo_url, asset_id, asset_id)
+        if branch is not None:
+            twigs_cmd = twigs_cmd + " --branch '%s'" % branch
+        if code_sharing == False:
+            twigs_cmd = twigs_cmd + " --no_code"
+        print("Running SAST checks for repo [%s] and branch [%s]" % (repo_url, branch))
+        try:
+            out = subprocess.check_output([twigs_cmd], stderr=dev_null_device, shell=True)
+            print("SAST completed")
+        except subprocess.CalledProcessError as e:
+            print("Error running twigs SAST checks")
+            print(e)
+            return False
+
+    return True
+
+def set_requests_verify(verify):
+    global GoDaddyCABundle
+    GoDaddyCABundle = verify
+
+def get_requests_verify():
+    global GoDaddyCABundle
+    config = get_config()
+    ssl_verification = config['threatworx'].getboolean('ssl_verification', fallback=True)
+    if ssl_verification:
+        return GoDaddyCABundle
+    else:
+        return False
+
+def launch_request_handler_process(python_script_name, event_data):
+    temp_json_file = tempfile.NamedTemporaryFile(mode='w', prefix='tw-', suffix='_ed.json', delete=False)
+    temp_json_file_name = temp_json_file.name
+    json.dump(event_data, temp_json_file)
+    temp_json_file.close()
+    base_path = os.path.dirname(os.path.realpath(__file__))
+    cmd = base_path + os.sep + python_script_name + ' -f ' + temp_json_file_name
+    proc = subprocess.Popen([cmd], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
+
+def process_push_request(event_data):
+    launch_request_handler_process('push_request_handler', event_data)
